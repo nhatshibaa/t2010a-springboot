c43ef9cf781597163d00e92f3eff1d9615d226fb
Nhật Nguyễn Tiến <kaotuong113@gmail.com>
2022-06-07 15:38:12 +0700

    add cross origin

--- src/main/java/com/example/t2010aspringboot/controller/ProductController.java
+++ src/main/java/com/example/t2010aspringboot/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
--- src/main/java/com/example/t2010aspringboot/controller/StudentController.java
+++ src/main/java/com/example/t2010aspringboot/controller/StudentController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
